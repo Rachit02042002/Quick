ff56a681f3dc446574b5aff468ba07921071d390
RACHIT DIXIT <rachit619.rd@gmail.com>
2024-04-05 16:12:47 +0530

    updated

--- backend/db.js
+++ backend/db.js
@@ -1,61 +1,28 @@
-// const mongoose = require('mongoose')
-// const mongoURI = 'mongodb+srv://FoodDelivery:food@cluster0.zkrlxyt.mongodb.net/FoodDelivery?retryWrites=true&w=majority'
-
-
-// const mongoDB = async()=>{
-//    await mongoose.connect(mongoURI).then((data)=>{
-//         console.log(`Mongodb connected with server`);
-//         const fetched_data = mongoose.connection.db.collection("food_items");
-//         fetched_data.find({}).toArray(function(err,data){
-//             if(err)console.log(err);
-//             else console.log(data);
-//         })
-//     }).catch((err)=>{
-//         console.log(err)
-//     })
-// }
-
-// module.exports = mongoDB;
-
-
 const mongoose = require('mongoose');
 const mongoURI = 'mongodb+srv://FoodDelivery:food@cluster0.zkrlxyt.mongodb.net/FoodDelivery?retryWrites=true&w=majority';
 
 const mongoDB = async () => {
     try {
-        // Connect to MongoDB
+        
         await mongoose.connect(mongoURI, {
             useNewUrlParser: true,
             useUnifiedTopology: true
         });
         console.log(`MongoDB connected`);
 
-        //Access the "food_items" collection
+       
         const collection = mongoose.connection.db.collection("food_items");
         const collection1 = mongoose.connection.db.collection("foodCategory");
 
-        // Fetch data from the collection
+        
         const data = await collection.find({}).toArray();
         const data1 = await collection1.find({}).toArray();
 
-        // Display the fetched data
+      
 
         global.food_items = data;
         global.foodCategory = data1;
 
-        // const fetched_data = await mongoose.connection.db.collection("food_items");
-        // fetched_data.find({}).toArray(async function (err,data){
-        //     const foodCategory = await mongoose.connection.db.collection("foodCategory");
-        //     foodCategory.find({}).toArray(function(err,catData){
-        //         console.log(data);
-        //         console.log(catData);
-        //         if(err)console.log(err);
-        //         else{
-        //             global.food_items=data;
-        //             global.foodCategory = catData;
-        //         }
-        //     })
-        // })
         
     } catch (error) {
         console.error('MongoDB connection error:', error);
--- src/screens/MyOrder.js
+++ src/screens/MyOrder.js
@@ -9,8 +9,7 @@ export default function MyOrder() {
     const fetchMyOrder = async () => {
         console.log(localStorage.getItem('userEmail'))
         await fetch("http://localhost:5000/api/myOrderData", {
-            // credentials: 'include',
-            // Origin:"http://localhost:3000/login",
+         
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
@@ -22,13 +21,6 @@ export default function MyOrder() {
             let response = await res.json()
             await setorderData(response)
         })
-
-
-
-        // await res.map((data)=>{
-        //    console.log(data)
-        // })
-
 
     }
 
@@ -61,7 +53,6 @@ export default function MyOrder() {
 
                                                         <div className='col-12 col-md-6 col-lg-3' >
                                                             <div className="card mt-3" style={{ width: "16rem", maxHeight: "360px" }}>
-                                                                {/* <img src={arrayData.img} className="card-img-top" alt="..." style={{ height: "120px", objectFit: "fill" }} /> */}
                                                                 <div className="card-body">
                                                                     <h5 className="card-title">{arrayData.name}</h5>
                                                                     <div className='container w-100 p-0' style={{ height: "38px" }}>
@@ -98,4 +89,3 @@ export default function MyOrder() {
         </div>
     )
 }
-// {"orderData":{"_id":"63024fd2be92d0469bd9e31a","email":"mohanDas@gmail.com","order_data":[[[{"id":"62ff20fbaed6a15f800125e9","name":"Chicken Fried Rice","qty":"4","size":"half","price":520},{"id":"62ff20fbaed6a15f800125ea","name":"Veg Fried Rice","qty":"4","size":"half","price":440}],"2022-08-21T15:31:30.239Z"],[[{"id":"62ff20fbaed6a15f800125f4","name":"Mix Veg Pizza","qty":"4","size":"medium","price":800},{"id":"62ff20fbaed6a15f800125f3","name":"Chicken Doub;e Cheeze Pizza","qty":"4","size":"regular","price":480}],"2022-08-21T15:32:38.861Z"]],"__v":0}}
